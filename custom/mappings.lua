1d151ae6de10be24c1cdf24b5afb3c7d49b5ca22
Pan Shaun <Shaun.Pan@fil.com>
2023-02-03 14:25:10 +0800

    1. change key map for toggle searching files in telescope. 2. update to use nvim-tree/nvim-tree.lua plugin

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -110,6 +110,7 @@ M.telescope = {
 	n = {
 		-- toggle
 		["<leader>ts"] = { "<cmd> Telescope <CR>", "toggle telescope" },
+		["<leader>bb"] = { "<cmd> Telescope buffers <CR>", "find buffers" },
 	},
 }
 
