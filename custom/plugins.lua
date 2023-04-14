d57a9bf1e722b78475861f9214d334b12914149b
Pan Shaun <Shaun.Pan@fil.com>
2023-04-15 00:11:15 +0800

    remove sneak and add flit/leap.nvim for better motion add noice.nvim but still having issue: https://github.com/NvChad/NvChad/issues/1656

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -104,7 +104,68 @@ local plugins = {
 	{ "rainbowhxch/accelerated-jk.nvim", lazy = false },
 	{ "ThePrimeagen/harpoon", lazy = false },
 	{ "tpope/vim-repeat", lazy = false },
-	{ "justinmk/vim-sneak", lazy = false },
+	-- { "justinmk/vim-sneak", lazy = false },
+	-- easily jump to any location and enhanced f/t motions for Leap
+	{
+		"ggandor/flit.nvim",
+		keys = function()
+			---@type LazyKeys[]
+			local ret = {}
+			for _, key in ipairs({ "f", "F", "t", "T" }) do
+				ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+			end
+			return ret
+		end,
+		opts = { labeled_modes = "nx" },
+	},
+	{
+		"ggandor/leap.nvim",
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+			{ "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+			{ "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+		},
+		config = function(_, opts)
+			local leap = require("leap")
+			for k, v in pairs(opts) do
+				leap.opts[k] = v
+			end
+			leap.add_default_mappings(true)
+			vim.keymap.del({ "x", "o" }, "x")
+			vim.keymap.del({ "x", "o" }, "X")
+		end,
+	},
+
+	--   -- ui components
+	{ "MunifTanjim/nui.nvim" },
+	-- noicer ui
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["vim.lsp.hover.enabled"] = false,
+				},
+			},
+			presets = {
+				bottom_search = true,
+				command_palette = true,
+				long_message_to_split = true,
+			},
+		},
+    -- stylua: ignore
+    keys = {
+      { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+      { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+      { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+      { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+      { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
+      { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
+    },
+	},
 	{ "tpope/vim-surround", lazy = false },
 	{ "tpope/vim-unimpaired", lazy = false },
 	{ "tpope/vim-fugitive", lazy = false },
