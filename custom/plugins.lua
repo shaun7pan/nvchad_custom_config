c4bd172b2803aabe5c6d71916071078878671c8b
Pan Shaun <Shaun.Pan@fil.com>
2023-04-15 14:33:27 +0800

    Add nvim-notify for better notify.

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -166,6 +166,31 @@ local plugins = {
       { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
     },
 	},
+	{
+		"rcarriga/nvim-notify",
+		keys = {
+			{
+				"<leader>un",
+				function()
+					require("notify").dismiss({ silent = true, pending = true })
+				end,
+				desc = "Delete all Notifications",
+			},
+		},
+		opts = {
+			timeout = 3000,
+			max_height = function()
+				return math.floor(vim.o.lines * 0.75)
+			end,
+			max_width = function()
+				return math.floor(vim.o.columns * 0.75)
+			end,
+		},
+		init = function()
+			-- when noice is not enabled, install notify on VeryLazy
+			vim.notify = require("notify")
+		end,
+	},
 	{ "tpope/vim-surround", lazy = false },
 	{ "tpope/vim-unimpaired", lazy = false },
 	{ "tpope/vim-fugitive", lazy = false },
